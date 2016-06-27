12b4e563ef2271441d8369940b7c1b5dc655fb80
Chris Katnic <chris.katnic@gmail.com>
2016-06-27 16:59:24 -0700

    init

--- DMRestTemplate/ViewController.swift
+++ DMRestTemplate/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dataLabel: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+ 
+    @IBAction func callClicked(sender: AnyObject) {
+        
+        self.dataLabel.text?.appendContentsOf("\nthis")
+        print("this")
     }
 
 
